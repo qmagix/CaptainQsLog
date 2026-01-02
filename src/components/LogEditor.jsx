085f886c133e82c245b70c0425270ed1b6535c55
Q Huang <q@hopelab.com>
2026-01-01 18:58:51 -0800

    moving buttons to top make it more usable in mobile

--- src/components/LogEditor.jsx
+++ src/components/LogEditor.jsx
@@ -6,6 +6,7 @@ import AudioRecorder from './AudioRecorder';
 import MediaManager from './MediaManager';
 import './LogEditor.css';
 import './AudioRecorder.css';
+import './LogEditorControls.css';
 
 const LogEditor = () => {
     const { activeLog, updateLog, setActiveLogId } = useLogs();
@@ -42,6 +43,20 @@ const LogEditor = () => {
                 </div>
             </div>
 
+            <div className="editor-controls">
+                <MediaManager
+                    images={activeLog.images}
+                    videos={activeLog.videos}
+                    onUpdate={(updates) => updateLog(activeLog.id, updates)}
+                />
+
+                <AudioRecorder
+                    audioData={activeLog.audio}
+                    onSave={(audioBase64) => updateLog(activeLog.id, { audio: audioBase64 })}
+                    onDelete={() => updateLog(activeLog.id, { audio: null })}
+                />
+            </div>
+
             <textarea
                 ref={textareaRef}
                 className="log-textarea"
@@ -50,18 +65,6 @@ const LogEditor = () => {
                 placeholder="Begin log entry..."
                 autoFocus
                 spellCheck="false"
-            />
-
-            <MediaManager
-                images={activeLog.images}
-                videos={activeLog.videos}
-                onUpdate={(updates) => updateLog(activeLog.id, updates)}
-            />
-
-            <AudioRecorder
-                audioData={activeLog.audio}
-                onSave={(audioBase64) => updateLog(activeLog.id, { audio: audioBase64 })}
-                onDelete={() => updateLog(activeLog.id, { audio: null })}
             />
 
             <div className="editor-footer">
